5ef67213797d97a7b6f46532a3715e9c5a5b46e2
Kevin12-U <1719110155@utectulancingo.edu.mx>
2024-01-17 12:50:29 -0600

    condicionales

---
+++ initial/js/index.js
@@ -0,0 +1,16 @@
+alert("Hola mundo"); /* no afecta usar ; 
+                        pero, es una buena practica de programación*/
+let numeroSecreto = 6;
+/* prompt insertal informacion */
+/* nomenclatura camel case para variables */
+let numeroUsuairo = prompt("Dame un numero: ");
+/* forma 1 */
+alert(numeroUsuairo);
+/* forma 2: desde consola */
+console.log(numeroUsuairo);
+
+/* comparacion con if */
+
+if (numeroUsuairo == numeroSecreto) {
+    alert('el numero es correcto')
+}
