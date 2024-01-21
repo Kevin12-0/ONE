cc70ab848828dbcefc5f589684da4672f1aefbc1
Kevin12-U <1719110155@utectulancingo.edu.mx>
2024-01-20 18:36:00 -0600

    funciones e inner html

---
+++ 2.LDP-Listas&Funiones/initial/js/app.js
@@ -0,0 +1,11 @@
+/* esto es para generar texto seleccionando una equiqueta */
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Juego del numero secreto";
+/* esto es para generara texto seleccionando una etiqueta */
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Indica un numero del 1 al 10";
+
+/* crear una funcion */
+function intentoDeUsuario() {
+  alert("Click desde el boton");
+}
