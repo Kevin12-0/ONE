e683a34f05de8bbf88442d1f9fb2270fb6630f01
Kevin12-U <1719110155@utectulancingo.edu.mx>
2024-01-22 18:41:29 -0600

    funcionalidad de nuevo juego

--- 2.LDP-Listas&Funiones/initial/js/app.js
+++ 2.LDP-Listas&Funiones/initial/js/app.js
@@ -1,16 +1,34 @@
 let numeroSecreto = generarNumeroSecreso(); /* variable global */
-
+let intentos = 1;
 /* crear una funcion */
 function verificarIntento() {
   /* capturara el numero del usuario insertado en el input  */
   let numeroDeUsuario = parseInt(
     document.getElementById("valorUsuario").value
   ); /* cambiar tipo de dato */
-  console.log(typeof numeroDeUsuario); /* verificar el tipo de dato realizado */
-  console.log(numeroSecreto);
-  console.log(numeroDeUsuario);
+
   console.log(numeroDeUsuario === numeroSecreto); /* retorna un boleano */
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento(
+      "p",
+      `Lo lograste, lo hiciste en ${intentos} ${
+        intentos == 1 ? "vez" : "veces"
+      }`
+    );
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else if (numeroDeUsuario > numeroSecreto) {
+    /* el usuario no acerto */
+    asignarTextoElemento("p", "el numero secreto es menor");
+  } else if (numeroDeUsuario < numeroSecreto) {
+    asignarTextoElemento("p", "el numero secreto es mayor");
+  }
+  intentos++;
+  limpiarInput();
   return;
+}
+
+function limpiarInput() {
+  document.querySelector("#valorUsuario").value = "";
 }
 
 function asignarTextoElemento(elemento, texto) {
@@ -24,6 +42,23 @@ function generarNumeroSecreso() {
   return numeroSecreto;
 }
 
-/* llamar a funcion */
-asignarTextoElemento("h1", "Juego del numero secreto!");
-asignarTextoElemento("p", "Indica un nuemro del 1 al 10");
+function condicionesIniciales() {
+  /* llamar a funcion */
+  /* generar el numero aleatorio */
+  /* intentos reiniciar */
+  asignarTextoElemento("h1", "Juego del numero secreto!");
+  /* mesaje de inicio del intervalo de numeros */
+  asignarTextoElemento("p", "Indica un nuemro del 1 al 10");
+  numeroSecreto = generarNumeroSecreso();
+  intentos = 1;
+}
+function reiniciarJuego() {
+  /* limpiar la caja */
+  limpiarInput();
+  condicionesIniciales();
+
+  /* desavilitar el boton */
+  document.querySelector("#reiniciar").setAttribute("disabled", "true");
+}
+
+condicionesIniciales();
